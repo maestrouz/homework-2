2a68688c0b2502111b4401dc22b3fd23683fe6f1
maestrouz <tsrichpubgm@gmail.com>
2023-08-23 02:04:01 +0500

    hw2

---
+++ main.js
@@ -0,0 +1,70 @@
+// 1-masala
+// let k = 5;
+// let n = 3;
+// for (let i=0; i<=n; i++) {
+//     console.log(k);
+// }
+
+
+// 2-masala
+// let a=10, b=1;
+// for(let son=b+1; son<a; son++){
+//     console.log(son);
+// }
+
+
+
+// 3-masala
+// function check(a,b){
+//     for (let i=a-1; i>b ; i--){
+//         console.log(i);
+//     }
+// }
+// check(10,5)
+
+
+// 4-masala
+// let konfet = 10000
+// for (let i = 1; i <= 10; i ++){
+//     console.log(`konfet ${konfet} som`);
+//     konfet += 10000
+// }
+
+
+
+// 5-masala
+// let  konfet=10000;{
+//     for (let i=0.1; konfet-0.1; i++){
+//         console.log(konfet);
+//     }
+// }
+
+
+// 6-masala
+// let konfet = 10000;
+// for (let i = 1; i <= 5; i ++){
+//     console.log(`kofet ${konfet} som`);
+//     konfet += 2500
+// }
+
+
+// 11-masala
+// let n = 15;
+// for (let i=1; i<n; i++) {
+//     if (i%2 !=0) console.log(i);
+// }
+
+
+// 12-masala
+// let n = 18
+// for (let i=1; i<n; i++) {
+//     if (i%2==0) console.log(i);
+// }
+
+
+// 15-masala
+// let n = 5
+// for (let i=n; i>0; i--){
+//     console.log(i);
+// }
+
